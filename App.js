0350426cd266f502293edfccec42c527d50c2d3c
lucas-bresson <contact.bresson@gmail.com>
2020-01-21 18:18:18 +0100

    replaced static data by hackernews 10 articlesapi

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import { Card, Button } from 'react-native-elements';
+import { Linking } from 'expo';
+import { useFetch } from './src/hooks';
 import Deck from './src/components/Deck';
-import data from './src/data';
 
-const App = () => {
-  const renderCard = item => (
-    <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
-      <Text style={styles.text}>Lorem ipsum</Text>
-      <Button
-        icon={{ name: 'code' }}
-        backgroundColor="#03A9F4"
-        title="View now"
-      />
-    </Card>
-  );
+function App() {
+  const stories = useFetch('https://news-proxy-server.appspot.com/topstories', []);
+
+  const renderCard = ({ id, by, title, url }) => {
+    return (
+      <Card
+        key={id}
+        title={title}
+        titleNumberOfLines={1}
+        image={{
+          uri: 'https://avatars1.githubusercontent.com/u/4703068?s=400&v=4',
+        }}
+      >
+        <Text style={styles.text}>author: {by}</Text>
+        <Button
+          icon={{ name: 'link' }}
+          backgroundColor="#03A9F4"
+          title="Open in browser"
+          onPress={() => Linking.openURL(url)}
+        />
+      </Card>
+    );
+  };
 
   const renderNoMoreCards = () => (
     <Card title="All done!">
@@ -25,14 +38,10 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Deck
-        data={data}
-        renderCard={renderCard}
-        renderNoMoreCards={renderNoMoreCards}
-      />
+      <Deck data={stories} renderCard={renderCard} renderNoMoreCards={renderNoMoreCards} />
     </SafeAreaView>
   );
-};
+}
 
 export default App;
 
---
+++ src/hooks.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+export const useFetch = (url, initialValue) => {
+  const [result, setResult] = useState(initialValue);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => setResult(json));
+  }, []);
+
+  return result;
+};
